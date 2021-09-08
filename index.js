160231280c5713366ac6660d942ca2c9817a55ca
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-08 17:14:16 +0200

    Add readline question in the begging (requests input)

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import fs from 'node:fs';
+import chalk from 'chalk';
 import fetch from 'node-fetch';
+import readline from 'readline-sync';
+
+let answerArray = [];
 
 // create directory "memes" if it does not exist yet
 
 if (!fs.existsSync('memes')) {
   fs.mkdirSync('memes');
 }
+console.log(chalk.red.bold.bgBlue(`\nNode.js Meme Scraper\n\n`));
+const answer = readline.question(
+  `Press Enter to download 10 memes.\nYou can also enter three words to create a custom meme. (Format: top, bottom, meme-template)\n`,
+);
 
 // stretch goal
 
-if (process.argv[2]) {
-  if (process.argv[3] && process.argv[4]) {
-    const url = `https://api.memegen.link/images/${process.argv[4]}/${process.argv[2]}/${process.argv[3]}`;
-    const customFilename = `memes/custom_${process.argv[4]}_${process.argv[2]}_${process.argv[3]}.png`;
+if (answer) {
+  answerArray = answer.split(' ');
+  console.log(answerArray);
+
+  if (answerArray[1] && answerArray[2]) {
+    const url = `https://api.memegen.link/images/${answerArray[2]}/${answerArray[0]}/${answerArray[1]}`;
+    const customFilename = `memes/custom_${answerArray[0]}_${answerArray[1]}_${answerArray[2]}.png`;
     fetch(url)
       .then((response) => {
         response.body.pipe(fs.createWriteStream(customFilename));
@@ -39,14 +50,13 @@ if (process.argv[2]) {
         let jpgArray = pageString.match(
           /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
         );
-        console.log(jpgArray); // contains the pics twice (for some reason)
+        // jpgArray contains the pics twice (for some reason)
 
         // get rid of every other element
         jpgArray = jpgArray.filter((element, index) => {
           return index % 2 === 0;
         });
         jpgArray = jpgArray.slice(0, 10); // get first 10 elements
-        console.log(jpgArray);
 
         // save the images
 
