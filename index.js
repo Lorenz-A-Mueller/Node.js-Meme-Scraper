fa24f257ff62c7d16f12c39c41bba143f614c515
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-08 15:45:33 +0200

    Add Error Handling

--- index.js
+++ index.js
@@ -9,32 +9,39 @@ fetch('https://memegen-link-examples-upleveled.netlify.app/')
     try {
       pageString = JSON.stringify(text);
       console.log(pageString);
+
+      // look for https://api.memegen.link/images...........jpg
+
       let jpgArray = pageString.match(
         /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
       );
-      console.log(jpgArray); // why doubled?
+      console.log(jpgArray); // contains the pics twice (for some reason)
 
+      // get rid of every other element
       jpgArray = jpgArray.filter((element, index) => {
         return index % 2 === 0;
       });
-      console.log(jpgArray);
-
-      jpgArray = jpgArray.slice(0, 10); // it contains 10 pics now
-
+      jpgArray = jpgArray.slice(0, 10); // get first 10 elements
       console.log(jpgArray);
 
       // save the images
 
       for (let i = 0; i < 10; i++) {
-        let filename = 'memes/image_no_' + [i + 1] + '.jpg'; // create filename
+        const filename = 'memes/image_no_' + [i + 1] + '.jpg'; // create filename
         console.log(filename);
-        console.log('memes/image.jpg');
         console.log(jpgArray[i]);
-        fetch(jpgArray[i]).then((response) => {
-          response.body.pipe(fs.createWriteStream(filename));
-        });
+        fetch(jpgArray[i])
+          .then((response) => {
+            response.body.pipe(fs.createWriteStream(filename));
+          })
+          .catch(() => {
+            throw new Error('Cannot download memes');
+          });
       }
-    } catch (err) {
-      console.log(err);
+    } catch {
+      throw new Error('Cannot load data from URL');
     }
+  })
+  .catch(() => {
+    throw new Error('Could not find URL');
   });
