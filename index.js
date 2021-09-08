20b72bf7175ad0a22cba66f9ec6b62702cc3767b
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-08 14:28:27 +0200

    Add regex; array contains the 10 pics

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import fetch from 'node-fetch';
 
+let pageString = '';
+
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.text())
   .then((text) => {
     try {
-      console.log(JSON.stringify(text));
+      pageString = JSON.stringify(text);
+      console.log(pageString);
+      let jpgArray = pageString.match(
+        /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
+      );
+      console.log(jpgArray); // why doubled?
+
+      jpgArray = jpgArray.filter((element, index) => {
+        return index % 2 === 0;
+      });
+      console.log(jpgArray);
+
+      jpgArray = jpgArray.slice(0, 10); // it contains 10 pics now
+
+      console.log(jpgArray);
     } catch (err) {
       console.log(err);
     }
