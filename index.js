482b6fe8042c0aa34f79e3f090314430c515919e
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-08 16:31:17 +0200

    First stretch goal complete

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 import fs from 'node:fs';
 import fetch from 'node-fetch';
 
-let pageString = '';
+// stretch goal
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((text) => {
-    try {
-      pageString = JSON.stringify(text);
-      console.log(pageString);
+if (process.argv[2]) {
+  if (process.argv[3] && process.argv[4]) {
+    const url = `https://api.memegen.link/images/${process.argv[4]}/${process.argv[2]}/${process.argv[3]}`;
+    const customFilename = `memes/custom_${process.argv[4]}_${process.argv[2]}_${process.argv[3]}.png`;
+    fetch(url).then((response) => {
+      response.body.pipe(fs.createWriteStream(customFilename));
+    });
+  } else {
+    console.log('Please give 3 inputs!');
+  }
 
-      // look for https://api.memegen.link/images...........jpg
+  // default program
+} else {
+  let pageString = '';
 
-      let jpgArray = pageString.match(
-        /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
-      );
-      console.log(jpgArray); // contains the pics twice (for some reason)
+  fetch('https://memegen-link-examples-upleveled.netlify.app/')
+    .then((res) => res.text())
+    .then((text) => {
+      try {
+        pageString = JSON.stringify(text);
 
-      // get rid of every other element
-      jpgArray = jpgArray.filter((element, index) => {
-        return index % 2 === 0;
-      });
-      jpgArray = jpgArray.slice(0, 10); // get first 10 elements
-      console.log(jpgArray);
+        // look for https://api.memegen.link/images...........jpg
 
-      // save the images
+        let jpgArray = pageString.match(
+          /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
+        );
+        console.log(jpgArray); // contains the pics twice (for some reason)
 
-      for (let i = 0; i < 10; i++) {
-        const filename = 'memes/image_no_' + [i + 1] + '.jpg'; // create filename
-        console.log(filename);
-        console.log(jpgArray[i]);
-        fetch(jpgArray[i])
-          .then((response) => {
-            response.body.pipe(fs.createWriteStream(filename));
-          })
-          .catch(() => {
-            throw new Error('Cannot download memes');
-          });
+        // get rid of every other element
+        jpgArray = jpgArray.filter((element, index) => {
+          return index % 2 === 0;
+        });
+        jpgArray = jpgArray.slice(0, 10); // get first 10 elements
+        console.log(jpgArray);
+
+        // save the images
+
+        for (let i = 0; i < 10; i++) {
+          const filename = 'memes/image_no_' + [i + 1] + '.jpg'; // create filename
+          console.log(`Creating File: ${filename}`);
+          fetch(jpgArray[i])
+            .then((response) => {
+              response.body.pipe(fs.createWriteStream(filename));
+            })
+            .catch(() => {
+              throw new Error('Cannot download memes');
+            });
+        }
+      } catch {
+        throw new Error('Cannot load data from URL');
       }
-    } catch {
-      throw new Error('Cannot load data from URL');
-    }
-  })
-  .catch(() => {
-    throw new Error('Could not find URL');
-  });
+    })
+    .catch(() => {
+      throw new Error('Could not find URL');
+    });
+}
