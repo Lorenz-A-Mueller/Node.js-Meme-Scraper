b0761c16cd39a6d887ec39e043d3695254077bfd
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-09-09 13:46:00 +0200

    Add not-commited files last time

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 import fs from 'node:fs';
 import chalk from 'chalk';
+import logUpdate from 'log-update';
 import fetch from 'node-fetch';
 import readline from 'readline-sync';
 
-let answerArray = [];
+let progValue = 0;
+let progBlockRepeat = 0;
 
 // create directory "memes" if it does not exist yet
 
 if (!fs.existsSync('memes')) {
   fs.mkdirSync('memes');
 }
-
-// s
+// Show name of program and ask for a template
 
 console.log(chalk.red.bold.bgBlue(`\nNode.js Meme Scraper\n\n`));
 const template = readline.question(
   `Press Enter to download 10 memes or type in a template\n`,
 );
 
-// get custom Meme
+// get custom Meme if template was entered
 
 if (template) {
-  const topText = readline.question(`Enter the top text\n`);
-  const bottomText = readline.question(`Enter the bottom text\n`);
+  let topText = readline.question(`Enter the top text\n`);
+  let bottomText = readline.question(`Enter the bottom text\n`);
+  topText = topText.replace(/ /g, '_'); // website will convert underscore into whitespace
+  bottomText = topText.replace(/ /g, '_');
+
   const url = `https://api.memegen.link/images/${template}/${topText}/${bottomText}`;
   const customFilename = `memes/custom_${template}_${topText}_${bottomText}.png`;
   fetch(url)
@@ -33,10 +37,9 @@ if (template) {
       throw new Error("Couldn't download your meme. Sorry!");
     });
 
-  // default program (get 10 memes from site)
+  // no template entered (get 10 memes from site)
 } else {
   let pageString = '';
-
   fetch('https://memegen-link-examples-upleveled.netlify.app/')
     .then((res) => res.text())
     .then((text) => {
@@ -48,9 +51,7 @@ if (template) {
         let jpgArray = pageString.match(
           /https:\/\/api\.memegen\.link\/images(.)*?\.jpg/g,
         );
-        // jpgArray contains the pics twice (for some reason)
-
-        // get rid of every other element
+        // jpgArray contains the pictures twice, so get rid of every other element
         jpgArray = jpgArray.filter((element, index) => {
           return index % 2 === 0;
         });
@@ -60,7 +61,21 @@ if (template) {
 
         for (let i = 0; i < 10; i++) {
           const filename = 'memes/image_no_' + [i + 1] + '.jpg'; // create filename
-          console.log(`Creating File: ${filename}`);
+
+          progValue += 1;
+          progBlockRepeat = '█' + '|█'.repeat(progValue - 1);
+          // console.log(`Creating File: ${filename}`);
+          logUpdate(
+            chalk.red.bold(
+              `\nProgress: ${progBlockRepeat} ${progValue * 10}% `,
+            ),
+          );
+
+          if (i === 9) {
+            console.log(chalk.red.bold.bgBlue(`\nDownload complete!\n`));
+            console.log('Saved 10 files to "/memes"\n');
+          }
+
           fetch(jpgArray[i])
             .then((response) => {
               response.body.pipe(fs.createWriteStream(filename));
